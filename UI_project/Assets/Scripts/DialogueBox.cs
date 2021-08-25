01e5bcfe4fefb4171428f19af50cab82a37cef28
Charles <cwolfe7@asu.edu>
2021-08-24 19:53:53 -0700

    added PopUp UI

---
+++ UI_project/Assets/Scripts/DialogueBox.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueBox : MonoBehaviour
+{
+    [SerializeField] GameObject image = null;
+    [SerializeField] GameObject imageName = null;
+
+    [SerializeField] Text[] messages = null;
+
+    private int messageIdx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        messageIdx = 2;
+        SetActiveText(messageIdx);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetActiveText(int idx)
+    {
+        foreach(Text message in messages)
+        {
+            message.gameObject.SetActive(false);
+        }
+
+        messages[idx].gameObject.SetActive(true);
+    }
+
+    public void ScrollRight()
+    {
+        if (messageIdx < messages.Length -1)
+        {
+            messageIdx += 1;
+            SetActiveText(messageIdx);
+        }
+
+        else
+        {
+            messageIdx = 0;
+            SetActiveText(messageIdx);
+        }
+    }
+
+    public void ScrollLeft()
+    {
+        if (messageIdx > 0)
+        {
+            messageIdx -= 1;
+            SetActiveText(messageIdx);
+        }
+
+        else
+        {
+            messageIdx = messages.Length - 1;
+            SetActiveText(messageIdx);
+        }
+    }
+}
---
+++ UI_project/Assets/Scripts/DialogueBoxFloatingCanvas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBoxFloatingCanvas : MonoBehaviour
+{
+    [SerializeField] DialogueBox dialogueBox = null;
+    [SerializeField] GameObject icon = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueBox.gameObject.SetActive(false);
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        dialogueBox.gameObject.SetActive(true);
+        icon.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        dialogueBox.gameObject.SetActive(false);
+        icon.SetActive(true);
+    }
+}
